fdee3ac9b20479ea901315f257c37b5927a139a9
Jieun Kang <ochoby@gmail.com>
2024-04-17 20:48:17 +0900

    first commit

---
+++ react/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../src/stories/Button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button label="primary" primary={true} />
+    </>
+  )
+}
+
+export default App
