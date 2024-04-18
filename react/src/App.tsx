740e27660eeeb029e33f09666be05250a0af5e6d
Jieun Kang <ochoby@gmail.com>
2024-04-18 09:48:52 +0900

    예시 조작 및 추가

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 import { Button } from "../src/stories/Button"
-import './App.css'
+import { Page } from "./stories/Page"
 
 function App() {
 
+  const [buttonName, setButtonName] = useState<string>("nothing to chosen")
+
   return (
     <>
-      <Button label="primary" primary={true} />
+      <div>{`chosen button: ${buttonName}`}</div>
+      <Button label="primary" primary={true} onClick={() => setButtonName("primary")} />
+      <Button label="large" size="large" onClick={() => setButtonName("large")} />
+      <Button label="secondary" onClick={() => setButtonName("secondary")} />
+      <Button primary={true} label="Warning" backgroundColor="red" onClick={() => setButtonName("Warning")} />
+      <Page />
     </>
   )
 }
--- react/src/stories/Button.stories.ts
+++ react/src/stories/Button.stories.ts
@@ -2,21 +2,17 @@ import type {Meta, StoryObj} from "@storybook/react";
 import {fn} from "@storybook/test";
 import {Button} from "./Button";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Example/Button",
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  tags: ["autodocs"], //문서 자동 생성
   argTypes: {
     backgroundColor: {control: "color"},
+    size: {control: "text"}, // radio로 변경하면 스토리북에서 radio 버튼으로 변경!
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {onClick: fn()},
 } satisfies Meta<typeof Button>;
 
@@ -27,27 +23,27 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: "Button",
+    label: "Primary Button",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: "Button",
+    label: "Secondary Button",
   },
 };
 
 export const Large: Story = {
   args: {
     size: "large",
-    label: "Button",
+    label: "Large Button",
   },
 };
 
 export const Small: Story = {
   args: {
     size: "small",
-    label: "Button",
+    label: "Small Button",
   },
 };
 
